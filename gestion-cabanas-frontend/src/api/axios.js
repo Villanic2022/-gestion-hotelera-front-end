a6437574b4eb3fa539b075b6c7e889b631734397
villan_cvideo <nicolas.villa@nubiral.com>
2025-12-19 18:48:54 -0300

    se modifican variables de entorno

--- gestion-cabanas-frontend/src/api/axios.js
+++ gestion-cabanas-frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api",
+    // baseURL: "http://localhost:8080/api",
+    baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 // Interceptor: agrega el JWT a cada request
