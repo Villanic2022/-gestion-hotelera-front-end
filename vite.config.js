2b2cca5be28902c1eb3e396d4802d18b463bc3f7
villan_cvideo <nicolas.villa@nubiral.com>
2025-12-20 16:35:57 -0300

    Revert: Remove render.yaml and simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Optimizaciones para reducir uso de memoria
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          pdf: ['html2canvas', 'jspdf'],
-          utils: ['axios']
-        }
-      }
-    }
-  },
   server: {
     proxy: {
       '/api': {
